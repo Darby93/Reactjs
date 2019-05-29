22a55b3c5a55db51c3596cf6a4b7f48a89ac00e4
DarbyJ <jony8674@naver.com>
2019-05-29 18:04:04 +0900

    Upload Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import Toc from "./components/toc";
+import Content from "./components/content";
+import Subject from "./components/subject";
 import './App.css';
+
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h3>Hello, React!</h3>
+        <Subject title="웹임!" sub="HI !"></Subject>
+        <Subject title="리엑트임!" sub="에휴 !"></Subject>
+        <Toc></Toc>
+        <Content title="HTML!" desc="HTML is HyperText !"></Content>
       </div>
     );
   }
